d6fd43c6b503238397fb8fae26fc3ddfa20dcb9d
Osarumen Alohan <cyrilalohan@gmail.com>
2023-06-27 22:17:49 +0100

    edit order details

--- src/pages/Retailer/Store/RetailerOrderStatus.tsx
+++ src/pages/Retailer/Store/RetailerOrderStatus.tsx
@@ -190,6 +190,7 @@ const RetailerOrderStatus = () => {
                 image: item.images[0],
               })) || []
             }
+            deliveryFee={order?.delivery_fee || 0}
           />
           <h4 className="font-[700] text-[1rem] leading-[1.5rem]">Address</h4>
           <p className="w-[11.75rem] text-[0.875rem] leading-[1.25rem] mb-20 capitalize">
